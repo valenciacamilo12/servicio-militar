5fdc3216a95e3c684fd4652f5cbd5b8759fc31ef
Gustavo A. Rodriguez <garodriguez263@gmail.com>
2019-08-20 21:14:15 -0500

    He configurado los archivos staticos y la mayoria de los templates a utilizar en el proyecto

--- servicioMilitar/settings.py
+++ servicioMilitar/settings.py
@@ -104,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Bogota'
 
 USE_I18N = True
 
@@ -119,3 +119,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+
+STATIC_ROOT = os.path.join(os.path.basename(BASE_DIR), 'static_env', 'static_root')
+MEDIA_ROOT = os.path.join(os.path.basename(BASE_DIR), 'static_env', 'media_root')
+
