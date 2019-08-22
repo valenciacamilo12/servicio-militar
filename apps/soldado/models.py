a7667a1e5ad07bd9415784927dbc0e8c89c42767
camilo valencia <camilo3341215@gmail.com>
2019-08-21 19:32:43 -0500

    los forms estan listos

--- apps/soldado/models.py
+++ apps/soldado/models.py
@@ -5,9 +5,14 @@ class Servicio(models.Model):
     id_servicio = models.AutoField(primary_key=True)
     descripcion = models.CharField(max_length=40)
 
+    def __str__(self):
+        return '{}'.format(self.descripcion)
 class Cuerpo(models.Model):
     id_cuerpo = models.AutoField(primary_key=True)
     denominacion = models.CharField(max_length=40)
+
+    def __str__(self):
+        return '{}'.format(self.denominacion)
 
 class Soldado(models.Model):
     id_soldado = models.AutoField(primary_key=True)
@@ -17,3 +22,6 @@ class Soldado(models.Model):
     servicio = models.ForeignKey(Servicio, blank=True, null=True, on_delete=models.CASCADE)
     cuerpo = models.ForeignKey(Cuerpo, blank=True, null=True, on_delete=models.CASCADE)
     compania = models.ForeignKey(Compania, blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} {}'.format(self.nombre, self.apellido)
